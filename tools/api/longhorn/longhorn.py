109aab3e0c239106165507215d873c8ac232adcf
maany <imptodefeat@gmail.com>
2022-10-28 01:09:36 +0200

    add longhorn installation step

--- tools/api/longhorn/longhorn.py
+++ tools/api/longhorn/longhorn.py
@@ -1,14 +1,17 @@
 import logging
-
+from pathlib import Path
 from api.core.base_configuration import BaseConfiguration
 from termcolor import colored
 
 class InstallLonghorn(BaseConfiguration):
-    def __init__(self, kubeconfig: str):
+    def __init__(self, kubeconfig: str, longhorn_values: str):
         super().__init__()
         self.kubeconfig = kubeconfig
+        self.longhorn_values = longhorn_values
         self.steps = [
-            self.add_longhorn_helm_repo
+            self.add_longhorn_helm_repo,
+            self.update_helm_repo,
+            self.install_longhorn_helm_chart,
         ]
 
     def add_longhorn_helm_repo(self, log_prefix: str):
@@ -19,3 +22,29 @@ class InstallLonghorn(BaseConfiguration):
     def update_helm_repo(self, log_prefix: str):
         self.log(log_prefix, colored("Updating Helm repo", "blue"), logging.INFO)
         self.run_process(["helm", "repo", "update"], log_prefix=log_prefix)
+
+    def install_longhorn_helm_chart(self, log_prefix: str):
+        self.log(log_prefix, colored("Installing Longhorn Helm chart", "blue"), logging.INFO)
+        self.run_process([
+            "helm", "install", "longhorn", "longhorn/longhorn", 
+            "-f", self.longhorn_values, 
+            "--create-namespace",
+            "-n", "longhorn-system"
+            ],
+            log_prefix=log_prefix
+        )
+
+class WatchLonghornEvents(BaseConfiguration):
+    def __init__(self, kubeconfig: str):
+        super().__init__()
+        self.kubeconfig = kubeconfig
+        self.steps = [
+            self.watch_longhorn_events,
+        ]
+
+    def watch_longhorn_events(self, log_prefix: str):
+        self.log(log_prefix, colored("Watching Longhorn events", "blue"), logging.INFO)
+        self.watch_namespace_events(
+            namespace="longhorn-system",
+            log_prefix=log_prefix
+        )
--- tools/k8s_admin_setup_utils/longhorn.py
+++ tools/k8s_admin_setup_utils/longhorn.py
@@ -1,20 +1,31 @@
-import click
-from api.longhorn.longhorn import InstallLonghorn
-from api.core.run_context import kube_proxy
 import os
+from pathlib import Path
+
+import click
+
+from api.longhorn.longhorn import InstallLonghorn, WatchLonghornEvents
+from api.core.run_context import kube_proxy
+
+longhorn_values_default_path = Path(__file__).parent.parent / "config" / "longhorn-values.yaml"
+
 
 @click.group()
 @click.option("--kubeconfig", 
     default=f"{os.environ.get('HOME', '~')}/.kube/config", 
     type=click.Path(exists=True), 
     help="Path to kubeconfig file")
+@click.option("--longhorn-values",
+    default=str(longhorn_values_default_path),
+    type=click.Path(exists=True),
+    help="Path to Longhorn Helm values file")
 @click.pass_context
-def cli(context, kubeconfig):
+def cli(context, kubeconfig, longhorn_values):
     """
     Install or Configure Longhorn Storage on a Kubernetes Cluster
     """
     context.obj = click.Context(cli)
     context.obj.kubeconfig = kubeconfig
+    context.obj.longhorn_values = longhorn_values
 
 @cli.command()
 @click.pass_obj
@@ -23,4 +34,18 @@ def install(ctx):
     Install Longhorn
     """
     with kube_proxy(kubeconfig=ctx.kubeconfig):
-        InstallLonghorn(kubeconfig=ctx.kubeconfig).run()
+        InstallLonghorn(
+            kubeconfig=ctx.kubeconfig, 
+            longhorn_values=ctx.longhorn_values
+        ).run()
+
+@cli.command()
+@click.pass_obj
+def watch_events(ctx):
+    """
+    Watch Longhorn events
+    """
+    with kube_proxy(kubeconfig=ctx.kubeconfig):
+        WatchLonghornEvents(
+            kubeconfig=ctx.kubeconfig
+        ).run()
