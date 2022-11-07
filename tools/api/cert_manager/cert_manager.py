ed973430fd092440e99587806dccdeb2ec8cc4e4
maany <imptodefeat@gmail.com>
2022-11-07 22:05:08 +0100

    install letsencrypt certificates

--- tools/api/cert_manager/cert_manager.py
+++ tools/api/cert_manager/cert_manager.py
@@ -1,3 +1,6 @@
+import tempfile
+import json
+
 from termcolor import colored
 
 from api.core.base_configuration import BaseConfiguration
@@ -8,7 +11,7 @@ class InstallCertManagerHelmChart(BaseConfiguration):
         super().__init__()
         self.kubeconfig = kubeconfig
         self.certmanager_values = certmanager_values
-        
+
         self.steps = [
             self.add_cert_manager_helm_repo,
             self.install_cert_manager_helm_chart,
@@ -23,27 +26,185 @@ class InstallCertManagerHelmChart(BaseConfiguration):
         self.run_process([
             "helm", "repo", "update"
         ], log_prefix=log_prefix)
-    
+
     def install_cert_manager_helm_chart(self, log_prefix: str):
-        self.log(log_prefix, colored("Installing Cert Manager Helm Chart with CRDs", "green"))
+        self.log(log_prefix, colored(
+            "Installing Cert Manager Helm Chart with CRDs", "green"))
         self.run_process([
             "helm", "install", "cert-manager", "jetstack/cert-manager",
             "--create-namespace", "--namespace", "cert-manager",
             "--values", f"{self.certmanager_values}"
         ], log_prefix=log_prefix)
 
+
+class CreateClusterIssuer(BaseConfiguration):
+    def __init__(self, kubeconfig: str, email: str, domain: str, cloudflare_token: str, letsencrypt_environment: str) -> None:
+        super().__init__()
+        self.kubeconfig = kubeconfig
+        self.email = email
+        self.domain = domain
+        self.cloudflare_token = cloudflare_token
+
+        self.secret_cloudflare_token = {
+            "apiVersion": "v1",
+            "kind": "Secret",
+            "metadata": {
+                "name": "cloudflare-token-secret",
+                "namespace": "cert-manager"
+            },
+            "type": "Opaque",
+            "stringData": {
+                "cloudflare-token": f"{self.cloudflare_token}"
+            }
+        }
+        if letsencrypt_environment == "staging":
+            self.letsencrypt_acme_server = "https://acme-staging-v02.api.letsencrypt.org/directory"
+        elif letsencrypt_environment == "production":
+            self.letsencrypt_acme_server = "https://acme-v02.api.letsencrypt.org/directory"
+
+        self.cluster_issuer = {
+            "apiVersion": "cert-manager.io/v1",
+            "kind": "ClusterIssuer",
+            "metadata": {
+                "name": f"letsencrypt-{letsencrypt_environment}"
+            },
+            "spec": {
+                "acme": {
+                    "email": f"{self.email}",
+                    "server": f"{self.letsencrypt_acme_server}",
+                    "privateKeySecretRef": {
+                        "name": f"letsencrypt-{letsencrypt_environment}"
+                    },
+                    "solvers": [
+                        {
+                            "dns01": {
+                                "cloudflare": {
+                                    "email": f"{self.email}",
+                                    "apiTokenSecretRef": {
+                                        "name": "cloudflare-token-secret",
+                                        "key": "cloudflare-token"
+                                    }
+                                }
+                            },
+                            'selector': {
+                                'dnsZones': [f"{self.domain}"]
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+
+        self.steps = [
+            self.create_cloudflare_token_secret,
+            self.create_cluster_issuer
+        ]
+
+    def create_cloudflare_token_secret(self, log_prefix: str):
+        self.log(log_prefix, colored(
+            "Creating Cloudflare Token Secret", "green"))
+        self.log(log_prefix, json.dumps(
+            self.secret_cloudflare_token, indent=4))
+        with tempfile.NamedTemporaryFile(mode="w") as f:
+            json.dump(self.secret_cloudflare_token, f)
+            f.flush()
+            self.run_process([
+                "kubectl", "apply", "-f", f.name
+            ], log_prefix=log_prefix)
+
+    def create_cluster_issuer(self, log_prefix: str):
+        self.log(log_prefix, colored("Creating Cluster Issuer", "green"))
+        self.log(log_prefix, json.dumps(self.cluster_issuer, indent=4))
+        with tempfile.NamedTemporaryFile(mode="w") as f:
+            json.dump(self.cluster_issuer, f)
+            f.flush()
+            self.run_process([
+                "kubectl", "apply", "-f", f.name
+            ], log_prefix=log_prefix)
+
+
+class CreateCertificate(BaseConfiguration):
+    def __init__(self, kubeconfig: str, email: str, domain: str, letsencrypt_environment: str, local: bool, namespace: str) -> None:
+        super().__init__()
+        self.kubeconfig = kubeconfig
+        self.email = email
+        self.domain = domain
+        self.namespace = namespace
+        self.letsencrypt_environment = letsencrypt_environment
+        self.local = local
+        if self.local:
+            self.certificate_name = f"local-{self.domain.replace('.', '-')}"
+            self.secret_name = f"local-{self.domain.replace('.', '-')}-{self.letsencrypt_environment}-tls"
+            self.common_name = f"*.local.{self.domain}"
+            self.dns_names = [f"*.local.{self.domain}", f"local.{self.domain}"]
+        else:
+            self.certificate_name = f"{self.domain.replace('.', '-')}"
+            self.secret_name = f"{self.domain.replace('.', '-')}-{self.letsencrypt_environment}-tls"
+            self.common_name = f"*.{self.domain}"
+            self.dns_names = [f"*.{self.domain}", f"{self.domain}"]
+
+        self.certificate = {
+            "apiVersion": "cert-manager.io/v1",
+            "kind": "Certificate",
+            "metadata": {
+                "name": self.certificate_name,
+                "namespace": self.namespace
+            },
+            "spec": {
+                "secretName": self.secret_name,
+                "issuerRef": {
+                    "name": f"letsencrypt-{self.letsencrypt_environment}",
+                    "kind": "ClusterIssuer"
+                },
+                "commonName": self.common_name,
+                "dnsNames": self.dns_names
+            }
+        }
+
+        self.steps = [
+            self.create_certificate
+        ]
+
+    def create_certificate(self, log_prefix: str):
+        self.log(log_prefix, colored("Creating Certificate", "green"))
+        self.log(log_prefix, json.dumps(self.certificate, indent=4))
+        with tempfile.NamedTemporaryFile(mode="w") as f:
+            json.dump(self.certificate, f)
+            f.flush()
+            self.run_process([
+                "kubectl", "apply", "-f", f.name
+            ], log_prefix=log_prefix)
+
+
 class WatchCertManagerEvents(BaseConfiguration):
     def __init__(self, kubeconfig: str) -> None:
         super().__init__()
         self.kubeconfig = kubeconfig
-        
+
         self.steps = [
             self.watch_cert_manager_helm_chart,
         ]
 
     def watch_cert_manager_helm_chart(self, log_prefix: str):
-        self.log(log_prefix, colored("Watching Cert Manager Helm Chart", "green"))
+        self.log(log_prefix, colored(
+            "Watching Cert Manager Helm Chart", "green"))
         self.watch_namespace_events(
             namespace="cert-manager",
             log_prefix=log_prefix
         )
+
+class WatchChallenges(BaseConfiguration):
+    def __init__(self, kubeconfig: str, namespace: str) -> None:
+        super().__init__()
+        self.kubeconfig = kubeconfig
+        self.namespace = namespace
+
+        self.steps = [
+            self.watch_challenges
+        ]
+
+    def watch_challenges(self, log_prefix: str):
+        self.log(log_prefix, colored("Watching Challenges", "green"))
+        self.run_process([
+            "kubectl", "get", "challenges", "-w"
+        ], log_prefix=log_prefix)
--- tools/k8s_admin_setup_utils/cert_manager.py
+++ tools/k8s_admin_setup_utils/cert_manager.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import click
 
 from api.core.run_context import kube_proxy
-from api.cert_manager.cert_manager import InstallCertManagerHelmChart, WatchCertManagerEvents
+from api.cert_manager.cert_manager import InstallCertManagerHelmChart, WatchCertManagerEvents, CreateClusterIssuer, CreateCertificate, WatchChallenges
 
 
 certmanager_values_default_path = Path(
@@ -19,7 +19,7 @@ certmanager_values_default_path = Path(
 @click.pass_context
 def cli(context, kubeconfig):
     """
-    Install or Configure Traefik on a Kubernetes Cluster
+    Install or Configure cert manager on a Kubernetes Cluster
     """
     context.obj = click.Context(cli)
     context.obj.kubeconfig = kubeconfig
@@ -29,7 +29,7 @@ def cli(context, kubeconfig):
 @click.pass_obj
 def install(ctx):
     """
-    Install Traefik Helm Chart
+    Install Cert Manager
     """
     pass
 
@@ -38,7 +38,7 @@ def install(ctx):
 @click.option("--certmanager-values",
               default=str(certmanager_values_default_path),
               type=click.Path(exists=True),
-              help="Path to Traefik Helm values file")
+              help="Path to Cert Manager Helm values file")
 @click.pass_obj
 def helm_chart(ctx, certmanager_values):
     """
@@ -50,6 +50,48 @@ def helm_chart(ctx, certmanager_values):
     ).run()
 
 
+@install.command()
+@click.option("--email", required=True, help="Email address for Cloudflare")
+@click.option("--domain", required=True, help="Domain for Let's Encrypt managed by Cloudflare. For example: devmaany.com")
+@click.option("--cloudflare-api-token", required=True, help="Cloudflare API Token with Zone:DNS:Edit and Zone:Zone:Read permission")
+@click.option("--letsencrypt-environment", type=click.Choice(['staging', 'production']), help="Let's Encrypt Environment. For example: staging or production")
+@click.pass_obj
+def cluster_issuer(ctx, email, domain, cloudflare_api_token, letsencrypt_environment):
+    """
+    Install Cluster Issuer and Cloudflare Token as Secret
+    """
+    with kube_proxy(ctx.kubeconfig) as k:
+        CreateClusterIssuer(
+            kubeconfig=ctx.kubeconfig,
+            email=email,
+            domain=domain,
+            cloudflare_token=cloudflare_api_token,
+            letsencrypt_environment=letsencrypt_environment
+        ).run()
+
+
+@install.command()
+@click.option("--email", required=True, help="Email address for Cloudflare")
+@click.option("--domain", required=True, help="Domain for Let's Encrypt managed by Cloudflare. For example: devmaany.com")
+@click.option("--letsencrypt-environment", type=click.Choice(['staging', 'production']), help="Let's Encrypt Environment. For example: staging or production")
+@click.option("--namespace", default="default", help="Namespace where ingress is or will be deployed")
+@click.option("--local", is_flag=True, help="Configure certificate for local non-cloud cluster. Set to false for public accessible cluster")
+@click.pass_obj
+def certificate(ctx, email, domain, letsencrypt_environment, namespace, local):
+    """
+    Create Certificate for Ingress
+    """
+    with kube_proxy(ctx.kubeconfig) as k:
+        CreateCertificate(
+            kubeconfig=ctx.kubeconfig,
+            email=email,
+            domain=domain,
+            letsencrypt_environment=letsencrypt_environment,
+            local=local,
+            namespace=namespace
+        ).run()
+
+
 @cli.command()
 @click.pass_obj
 def watch(ctx):
@@ -59,4 +101,17 @@ def watch(ctx):
     with kube_proxy(ctx.kubeconfig) as k:
         WatchCertManagerEvents(
             kubeconfig=ctx.kubeconfig,
+        ).run()
+
+@cli.command()
+@click.option("--namespace", "-n", default="default", help="Namespace where ingress is or will be deployed")
+@click.pass_obj
+def watch_challenges(ctx, namespace):
+    """
+    Watch Certificates Challenges
+    """
+    with kube_proxy(ctx.kubeconfig) as k:
+        WatchChallenges(
+            kubeconfig=ctx.kubeconfig,
+            namespace=namespace
         ).run()
